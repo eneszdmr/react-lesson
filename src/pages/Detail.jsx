746d1eebf09175cb785f4a3c2d0df019069605c3
enesozdemirbts <enes.ozdemir@botas.gov.tr>
2024-01-26 17:17:49 +0300

    axios degerleri

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,24 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
-import { mydata } from "./Home";
+import {  useParams } from "react-router-dom";
+
 
 const Detail = () => {
   const { id } = useParams();
-  const [getData, setGetData] = useState(null);
-  const location = useLocation();
+ 
 
-  console.log(location," loca");
-  useEffect(() => {
-    if (id) {
-      setGetData(mydata.find((data) => data.id == id));
-    }
-  }, [id]);
+
 
   return (
     <div>
-      <h3>{location?.pathname}</h3>
-      <div>{getData?.name}</div>
-      <div>{getData?.description}</div>
+     detail page
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-export const mydata = [
-  { name: "telefon", description: "her türlü telefon modeli vardır", id: 1 },
-  { name: "televizon", description: "4k televziyon modelleri", id: 2 },
-  { name: "iphone", description: "iphone 15 pro max", id: 3 },
-  {
-    name: "derslik",
-    description: "en güzel derslik 4a sınıfı meslek lisesi",
-    id: 4,
-  },
-  { name: "kitaplik", description: "kütüphane millet ve ya milli ", id: 5 },
-];
 
 const Home = () => {
   const navigate = useNavigate();
+  const [myData, setMyData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+      const { data } = await axios.get("https://fakestoreapi.com/products");
+      setMyData(data);
+      setLoading(false);
+    };
+
+    getData();
+  }, []);
+  console.log(myData);
+
   return (
+    loading ? <div>Loading....</div> :
     <div>
       {
-      mydata.map((item, id) => (
-        <div
-          onClick={()=> navigate(`detail/${item.id}`)}
-          style={{ marginBottom: "10px", cursor: "pointer" }}
-          key={id}
-        >
-          <div >{item.name}</div>
-        </div>
-      ))
+        myData?.map((data,i) => (
+          <div key={i}>
+            <div>
+              {data.title}
+            </div>
+            <img src={data.image} />
+
+          </div>
+        ) )
       }
+
     </div>
-  );
+  )
 };
 
 export default Home;
